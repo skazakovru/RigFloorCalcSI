69bec26157e34f7e058b51eedb3ab6185d443b36
Sergei Kazakov <skazakovru@yahoo.com>
2017-08-17 12:12:28 +0300

    Initial Commit

---
+++ RigFloorCalcSI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RigFloorCalcSI
+//
+//  Created by Sergei Kazakov on 17/08/2017.
+//  Copyright © 2017 Sergei Kazakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
